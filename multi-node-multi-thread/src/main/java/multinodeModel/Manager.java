9326b6383ce30588ae75040d10a11271a37a9977
CrystalWang1225 <wang24@cooper.edu>
2021-02-20 23:04:08 -0500

    Coordinator v1

--- multi-node-multi-thread/src/main/java/multinodeModel/Manager.java
+++ multi-node-multi-thread/src/main/java/multinodeModel/Manager.java
@@ -4,18 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import graphModel.*;
+import messages.GlobalMinNode;
 import multithread.ManagerThread;
 
 public class Manager {
     private Graph graph;
-    private PriorityBlockingQueue<Node> globalQ = new PriorityBlockingQueue<Node>();
-    private List<Thread> threadList = new ArrayList<>();
+    private PriorityBlockingQueue<GlobalMinNode> globalQ = new PriorityBlockingQueue<>();
+    private AtomicBoolean isFinished;
+    private Node current ;
+    private List<Integer> visited = new ArrayList<Integer>();
+    private List<Integer> distance = new ArrayList<Integer>();
 
     public List<Integer> multiDijk(Graph graph, int numCores, int[] portList){
+        distance = new ArrayList<Integer>(graph.getNumberOfNodes());
+        //Initialize the distance to inifinity for every node except source node
+        for (int i = 0; i < graph.getNumberOfNodes(); i++){
+            distance.add(i, Integer.MAX_VALUE);
+        }
+        distance.set(graph.getSourceNode(), 0);
+
+        List<Thread> threadList = new ArrayList<>();
+        isFinished = new AtomicBoolean(false);
+        current = new Node(Integer.MIN_VALUE, Integer.MIN_VALUE);
         this.graph = graph;
-        CyclicBarrier cyclicBarrier = new CyclicBarrier();
+
+        MinNode minNode = new MinNode( globalQ, current, isFinished);
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(numCores, minNode);
+
         int start;
         int end = 0;
         //Distribution of the nodes to each cores
@@ -30,7 +48,8 @@ public class Manager {
                 leftOver --;
             }
             //spawn thread
-            Thread eachThread = new ManagerThread();
+            Thread eachThread = new Thread(new ManagerThread(graph,start,
+            end, cyclicBarrier,globalQ, portList[i], current, isFinished, visited));
             eachThread.start();
 
             threadList.add(eachThread);
@@ -44,9 +63,36 @@ public class Manager {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+        }
 
+        for (int i = 0; i < visited.size(); i++){
+            distance.set(i,visited.get(i));
+        }
+        return distance;
+    }
+
+    public static class MinNode implements Runnable{
+
+        private PriorityBlockingQueue<GlobalMinNode> globalMinNodes = new PriorityBlockingQueue<>();
+        private AtomicBoolean isFinished;
+
+        private Node current;
+        MinNode(PriorityBlockingQueue<GlobalMinNode> globalMinNodes, Node current, AtomicBoolean isFinished){
+            this.globalMinNodes = globalMinNodes;
+            this.current = current;
+            this.isFinished = isFinished;
 
         }
-        return new ArrayList<>();
+        @Override
+        public void run(){
+            if (globalMinNodes.size() == 0){
+                isFinished.set(true);
+
+            }else {
+                GlobalMinNode localMin = globalMinNodes.poll();
+                current.setNode(localMin.getNode());
+                current.setDistance(localMin.getDistance());
+            }
+        }
     }
 }
--- multi-node-multi-thread/src/main/java/multithread/ManagerThread.java
+++ multi-node-multi-thread/src/main/java/multithread/ManagerThread.java
@@ -1,46 +1,83 @@
 package multithread;
 
 import graphModel.*;
+import messages.GlobalMinNode;
+import messages.SetupData;
+import java.io.*;
+import java.net.Socket;
+import java.net.ServerSocket;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ManagerThread implements Runnable {
-    private Node prevNode;
-    private Node currentNode;
-    private Graph graph;
-    private ArrayList<Node> nodeList;
-    private PriorityBlockingQueue<Node> nodeQ;
-    public ManagerThread(Node prevNode, Node currentNode, Graph graph,
-                         ArrayList<Node> nodeList, PriorityBlockingQueue<Node> nodeQ){
-        this.prevNode = prevNode;
-        this.currentNode = currentNode;
-        this.graph = graph;
-        this.nodeList = nodeList;
-        this.nodeQ = nodeQ;
-    }
+   private Graph graph;
+   private CyclicBarrier cyclicBarrier;
+   private PriorityBlockingQueue<GlobalMinNode> globalMinNodes;
+   private int startNode;
+   private int endNode;
+   private int portNumber;
+   private Node current;
+   private AtomicBoolean isFinished;
+   private List<Integer> visited;
+
+   public ManagerThread(Graph graph, int startNode,
+                        int endNode, CyclicBarrier cyclicBarrier,
+                        PriorityBlockingQueue<GlobalMinNode> globalMinNodes,
+                        int portNumber, Node current, AtomicBoolean isFinished,
+                        List<Integer> visited){
+       this.graph = graph;
+       this.startNode = startNode;
+       this.endNode = endNode;
+       this.globalMinNodes = globalMinNodes;
+       this.cyclicBarrier = cyclicBarrier;
+       this.portNumber = portNumber;
+       this.current = current;
+       this.isFinished = isFinished;
+       this.visited = visited;
+   }
+
+   @Override
     public void run(){
-        int nodeID = currentNode.getNode();
-        for(int i = 0; i < graph.getNumberOfNodes(); i++){ // loops through available edges in ascending cost
-            Node nextNode = nodeList.get(i);
-            if(graph.getEdges().get(currentNode.getNode()).get(i) <= 0) continue;
-            int nextID = nextNode.getNode();
-            boolean casFinished = false;
-            // check and update distance if needed
-            while(!casFinished){
-                if(currentNode.getDistance() +
-                        graph.getEdges().get(nodeID).get(nextID)
-                        < nextNode.getDistance()){
-                    casFinished = nextNode.casDistance(nextNode.getDistance(),
-                            currentNode.getDistance() +
-                            graph.getEdges().get(nodeID).get(nextID));
-                }
-                else{
-                    break;
-                }
-            }
-            if(casFinished){
-                nodeQ.put(nextNode);
-            }
-        }
-    }
+       List<Integer> fromWorker = new ArrayList<>();
+       try(ServerSocket serverSocket = new ServerSocket(portNumber)){
+           System.out.println("Connecting to " + portNumber);
+           Socket socket = serverSocket.accept();
+
+           //Setting up the w/r with a client
+           ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+           ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+
+           objectOutputStream.writeObject(new SetupData(graph, startNode, endNode));
+           objectOutputStream.reset();
+
+           while (!isFinished.get()) {
+               GlobalMinNode response = (GlobalMinNode) objectInputStream.readObject();
+
+               objectOutputStream.writeObject(current);
+               objectOutputStream.reset();
+
+               globalMinNodes.add(response);
+
+               cyclicBarrier.wait();
+
+           }
+
+           objectOutputStream.writeObject(current);
+           objectOutputStream.reset();
+
+           visited = (List<Integer>) objectInputStream.readObject();
+           for (int i = startNode; i < endNode; i++){
+               visited.set(i,fromWorker.get(i));
+           }
+
+           socket.close();
+
+       } catch (IOException | InterruptedException | ClassNotFoundException e){
+           e.printStackTrace();
+       }
+   }
+
 }
