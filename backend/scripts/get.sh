ca50f362bd73565154b8336768cb8e47766c971a
kevinjiang11579 <jiang9@cooper.edu>
2021-05-12 13:22:51 -0400

    Updated login and signup to return whole item

---
+++ backend/scripts/get.sh
@@ -0,0 +1,1 @@
+curl https://0y6pzvqls1.execute-api.us-east-1.amazonaws.com/dev/todos/aaeb8510-a2ee-11eb-ad05-b7ed434c3d21
---
+++ backend/scripts/populateDonations.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+PROFILE="default"
+REGION=$(aws configure get region)
+PREAMBLE="--profile ${PROFILE} --region ${REGION}"
+TYPE=$1
+
+RESTAPI_ID=$(aws apigateway get-rest-apis ${PREAMBLE} | jq '.items | .[] | select(.name == "dev-ece465-final-bloodbank") | .id' | tr -d '"')
+STAGE_NAME=$(aws apigateway get-stages ${PREAMBLE} --rest-api-id ${RESTAPI_ID} | jq '.item | .[0] | .stageName' | tr -d '"')
+while IFS="" read -r p || [ -n "$p" ]
+do
+ jqs=$(jq -R 'split(",") | {"\(.[0])": "\(.[1])", "\(.[2])": "\(.[3])", "\(.[4])": "\(.[5])", "\(.[6])": "\(.[7])", "\(.[8])": "\(.[9])", "\(.[10])": "\(.[11])", "\(.[12])": "\(.[13])", "\(.[14])": "\(.[15])", "\(.[16])": "\(.[17])"}' <(echo "$p"))
+ curl -X POST -H "Content-Type: application/json" https://${RESTAPI_ID}.execute-api.${REGION}.amazonaws.com/${STAGE_NAME}/user/donate --data "$jqs"
+done < donationData2.txt
